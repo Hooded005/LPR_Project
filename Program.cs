d7df6b027c9f8439a35ea65c9dd6fe247a159857
Sean Botha <seanbotha2003@gmail.com>
2024-08-01 16:42:23 +0200

    Hello World 2

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Project
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Console.WriteLine("Hello World 2");
         }
     }
 }
